92595ba6675942fd84d2a3f431c15b47b435ad50
peter <857568344@qq.com>
2019-11-01 17:35:24 +0800

    1、IM 注册登录

---
+++ src/main/java/com/example/imnetty/ImNettyApplication.java
@@ -0,0 +1,52 @@
+package com.example.imnetty;
+
+import com.example.imnetty.commonhandler.ProtocolPacketHandler;
+import com.example.imnetty.commonhandler.SpliterHandler;
+import com.example.imnetty.protocol.server.LoginProtocolPacketHandler;
+import com.example.imnetty.protocol.server.RegisterProtocolPacketHandler;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class ImNettyApplication {
+
+    private static int inetPort = 8000;
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImNettyApplication.class, args);
+
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        new ServerBootstrap()
+                .group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
+                        nioSocketChannel.pipeline()
+                                .addLast(new SpliterHandler())
+                                .addLast(new ProtocolPacketHandler())
+                                .addLast(new RegisterProtocolPacketHandler())
+                        .addLast(new LoginProtocolPacketHandler())
+
+                        ;
+                    }
+                }).bind(inetPort).addListener(future -> {
+            if (future.isSuccess()) {
+                log.info("netty 服务端启动成功！！ 绑定端口：{}", inetPort);
+            }
+        });
+
+
+    }
+
+}
